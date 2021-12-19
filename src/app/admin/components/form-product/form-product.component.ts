cd6cedeedb90c2c3eca9b00a6f6e7c0fd7922e3c
Anabel Campagnoli <anitacampagnoli126@hotmail.com>
2021-12-19 01:09:06 -0400

    Fin del curso inicio de sesion y storage con firebase

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AngularFireStorage } from '@angular/fire/compat/storage'
+
+import { finalize } from 'rxjs/operators';
+
 import { MyValidators } from './../../../utils/validators'
 
 import { ProductsService } from 'src/app/core/services/products/products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -13,11 +18,13 @@ import { ProductsService } from 'src/app/core/services/products/products.service
 export class FormProductComponent implements OnInit {
 
   form!: FormGroup;
+  image$!: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage,
   ) { 
     this.buildForm();
   }
@@ -35,6 +42,26 @@ export class FormProductComponent implements OnInit {
         this.router.navigate(['./admin/products'])
       });
     }
+  }
+
+  uploadFile(event: Event){
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+    const name = 'image.png';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        this.image$ = fileRef.getDownloadURL();
+        this.image$.subscribe(url => {
+          console.log(url);
+          this.form.get('image')?.setValue(url);
+        });
+      })
+    )
+    .subscribe();
   }
 
   private buildForm(){
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
   url_api: 'http://platzi-store.herokuapp.com',
+  firebase : {
+    apiKey: 'AIzaSyBmroXmwM9Sn0_wZt0_u7AulD5KDBn5BTs',
+    authDomain: 'platzi-store-7964b.firebaseapp.com',
+    projectId: 'platzi-store-7964b',
+    storageBucket: 'platzi-store-7964b.appspot.com',
+    messagingSenderId: '825477001530',
+    appId: '1:825477001530:web:5d55b9bd158a0e9f752003'
+  }
 };
