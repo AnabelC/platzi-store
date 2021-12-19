f28ed42ee708ccce43146896e01d300687b5f775
Anabel Campagnoli <anitacampagnoli126@hotmail.com>
2021-12-19 01:14:04 -0400

    Fin del curso

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   url_api: 'http://platzi-store-stag.herokuapp.com',
-    firebase : {
-    apiKey: 'AIzaSyBmroXmwM9Sn0_wZt0_u7AulD5KDBn5BTs',
-    authDomain: 'platzi-store-7964b.firebaseapp.com',
-    projectId: 'platzi-store-7964b',
-    storageBucket: 'platzi-store-7964b.appspot.com',
-    messagingSenderId: '825477001530',
-    appId: '1:825477001530:web:5d55b9bd158a0e9f752003'
+  firebase : {
+    apiKey: '...',
+    authDomain: 'platzi-store-xyz.firebaseapp.com',
+    projectId: 'platzi-store-xyz',
+    storageBucket: 'platzi-store-xyz.appspot.com',
+    messagingSenderId: '00000000000',
+    appId: '1:000000000:web:000000000000'
   }
 };
